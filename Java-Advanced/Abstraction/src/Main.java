71254157f47d58a0e91ee6b0d8a7f02ee75fb7ea
Mirena Zhelyazkova <mimmy33@abv.bg>
2019-07-03 05:05:59 +0300

    Adding homework

--- Java-Advanced/Abstraction/src/Main.java
+++ Java-Advanced/Abstraction/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //pyramid
+
         int n = Integer.parseInt(scanner.nextLine());
 
         for (int row = 1; row <= n; row++) {
